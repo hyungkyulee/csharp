ffde125b2c574c36c87b26a1193c77648b6eda64
hyungkyu <hyungkyu@deepeyes.co.uk>
2020-05-18 23:48:36 +0100

    as data type

---
+++ GrammarBasic/GrammarBasic/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GrammarBasic
+{
+  class A
+  {
+    
+  }
+
+  class B
+  {
+    
+  }
+  
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("===== C# Basic Grammar =====");
+
+      string str1 = "123";
+      object obj = str1; // boxing
+      string str2 = obj as string;
+      Console.WriteLine(str2);
+
+      A test1 = new A();
+      object obj1 = test1;
+      B test2 = obj1 as B;
+      if(test2 == null)
+      {
+        Console.WriteLine("Failed to change the data type.");
+      }
+      else
+      {
+        Console.WriteLine("Success to change the data type.");
+      }
+    }
+  }
+}
